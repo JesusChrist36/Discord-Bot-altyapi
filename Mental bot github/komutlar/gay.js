564d81d4dc75b75d51bc70facabc8969aa3b3ed1
JesusChrist36 <69605814+JesusChrist36@users.noreply.github.com>
2022-04-22 18:32:52 +0300

    Add files via upload

--- Mental bot github/komutlar/gay.js
+++ Mental bot github/komutlar/gay.js
@@ -2,24 +2,24 @@ const Discord = require('discord.js')
 exports.run = async (client, message, args) => {
 
 var sayı = Math.floor(Math.random() * 100) + 1;
+const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
 
-    let embed = new Discord.MessageEmbed()
-      .setTitle(':rainbow_flag: Gay ölçer :rainbow_flag: ')
-      .setThumbnail(message.author.displayAvatarURL())
-      .setColor('RANDOM')
-      .addField(`:rainbow_flag: ${sayı}% Gaysin :rainbow_flag: `)
-      message.channel.send(embed)
-
-
-}
+  let embed = new Discord.MessageEmbed()
+    .setTitle(':rainbow_flag: Gay ölçer :rainbow_flag: ')
+    .setThumbnail(Member.user.displayAvatarURL())
+    .setColor('RANDOM')
+    .addField(`:rainbow_flag: ${Member.user.username}, ${sayı}% Gaysin :rainbow_flag: `)
+    message.channel.send(embed)
+ 
+};
 exports.help = {
     name: "gay",
-    description: "% kaç gay olduğunuzu söyler",
-    usage: "?gay",
-    example: "?gay"
+    description: ".",
+    usage: ".",
+    example: "."
 }
 
 exports.conf = {
-    aliases: ["lgbt"],
+    aliases: ['lgbt','g'],
     cooldown: 0
 }
