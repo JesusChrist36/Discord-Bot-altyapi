e5b7de3f55ce65583686c012c4267946fc0ef216
JesusChrist36 <69605814+JesusChrist36@users.noreply.github.com>
2022-05-01 10:01:54 +0300

    Update gay.js

--- Mental bot github/komutlar/gay.js
+++ Mental bot github/komutlar/gay.js
@@ -1,14 +1,14 @@
 const Discord = require('discord.js')
 exports.run = async (client, message, args) => {
 
-var sayı = Math.floor(Math.random() * 100) + 1;
+var sayı = Math.floor(Math.random() * 101) + 1;
 const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
 
   let embed = new Discord.MessageEmbed()
     .setTitle(':rainbow_flag: Gay ölçer :rainbow_flag: ')
     .setThumbnail(Member.user.displayAvatarURL())
     .setColor('RANDOM')
-    .addField(`:rainbow_flag: ${Member.user.username}, ${sayı}% Gaysin :rainbow_flag: `)
+    .addField(`**${Member.user.username}**`, `**:rainbow_flag: >${sayı}%< Gaysin :rainbow_flag: **`)
     message.channel.send(embed)
  
 };
